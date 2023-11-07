e1c97e9325da7c3cc1cc850ce69145fcb8fc53c1
norb_ <127006485+devnorb@users.noreply.github.com>
2023-11-07 12:01:53 -0800

    istg if this doesn't work ill kms

--- FINALE.lua
+++ FINALE.lua
@@ -1,22 +1,16 @@
 repeat
-    task.wait(1)
+    task.wait(0.5)
 until game:IsLoaded()
-while task.wait(0.03) do
-    for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
-        if v.Name == "Security" then
-            for i, v in game.ReplicatedStorage.Security:GetChildren() do
-                v:Destroy()
+
+pcall(function()
+    while task.wait(0.03) do
+        for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
+            if v.Name == "Security" then
+                for i, v in game.ReplicatedStorage.Security:GetChildren() do
+                    v:Destroy()
+                end
+                game.ReplicatedStorage.Security:Destroy()
             end
-            game.ReplicatedStorage.Security:Destroy()
         end
     end
-end
-task.spawn(function()
-    warn("revamped anti bac script - norb_ | ;p")
-    local notif = loadstring(game:HttpGet(
-        "https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Jailbreak/Notification/Source.lua"))()
-    notif.new({
-        Text = "check console if there is a info msg it should work - norb_ | happy exploiting",
-        Duration = 2
-    })
 end)
