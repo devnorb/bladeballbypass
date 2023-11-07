46d5a604c9cfc790c3ee47b3a4a861462af60cd8
norb_ <127006485+devnorb@users.noreply.github.com>
2023-11-07 11:52:43 -0800

    GFUCKOSDKASODKOASCJD

--- FINALE.lua
+++ FINALE.lua
@@ -1,16 +1,16 @@
 repeat
     task.wait(1)
 until game:IsLoaded()
-
-for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
-    if v.Name == "Security" then
-        for i, v in game.ReplicatedStorage.Security:GetChildren() do
-            v:Destroy()
+while task.wait(0.03) do
+    for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
+        if v.Name == "Security" then
+            for i, v in game.ReplicatedStorage.Security:GetChildren() do
+                v:Destroy()
+            end
+            game.ReplicatedStorage.Security:Destroy()
         end
-        game.ReplicatedStorage.Security:Destroy()
     end
 end
-
 task.spawn(function()
     warn("revamped anti bac script - norb_ | ;p")
     local notif = loadstring(game:HttpGet(
