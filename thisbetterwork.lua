14d165736e26a166d017850fdeba760d2e8535b3
norb_ <127006485+devnorb@users.noreply.github.com>
2023-11-07 19:10:12 -0800

    Update thisbetterwork.lua

--- thisbetterwork.lua
+++ thisbetterwork.lua
@@ -7,12 +7,10 @@ until game:IsLoaded()
 pcall(function()
     while task.wait(0.03) do
         for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
-            if v.Name == "Security" then
-                for i, v in game.ReplicatedStorage.Security:GetChildren() do
-                    v:Destroy()
-                end
-                game.ReplicatedStorage.Security:Destroy()
+            for i, v in game.ReplicatedStorage.Security:GetChildren() do
+                v:Destroy()
             end
+            game.ReplicatedStorage.Security:Destroy()
         end
     end
 end)
