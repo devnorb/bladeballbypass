2ab74a49369ecc9fabd0c3ef167e0e575176db2e
norb_ <127006485+devnorb@users.noreply.github.com>
2023-11-11 20:29:43 -0800

    Update thisbetterwork.lua

--- thisbetterwork.lua
+++ thisbetterwork.lua
@@ -1,17 +1,35 @@
-warn(" USE THE SOURCE CODE LOADSTRING GETS DETECTED I THINK - norb")
---[[
-repeat
-    task.wait(0.5)
-until game:IsLoaded()
 
-pcall(function()
-    while task.wait(0.03) do
-        for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
-            for i, v in game.ReplicatedStorage.Security:GetChildren() do
-                v:Destroy()
+if game.PlaceId == 13772394625 then
+    repeat
+        task.wait(0.5)
+    until game:IsLoaded()
+    queue_on_teleport([[
+        repeat
+            task.wait(0.5)
+        until game:IsLoaded()
+        pcall(function()
+            while task.wait(0.03) do
+                for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
+                    for i, v in game.ReplicatedStorage.Security:GetChildren() do
+                        v:Destroy()
+                    end
+                    game.ReplicatedStorage.Security:Destroy()
+                    game.ReplicatedStorage.Freeze:Destroy()
+                end
             end
-            game.ReplicatedStorage.Security:Destroy()
+        end)
+        warn("anticheat should be bypassed, try now - norb_")
+    ]])
+    pcall(function()
+        while task.wait(0.03) do
+            for i, v in pairs(game.ReplicatedStorage:GetChildren()) do
+                for i, v in game.ReplicatedStorage.Security:GetChildren() do
+                    v:Destroy()
+                end
+                game.ReplicatedStorage.Security:Destroy()
+                game.ReplicatedStorage.Freeze:Destroy()
+            end
         end
-    end
-end)
-]]--
+    end)
+    warn("anticheat should be bypassed, try now - norb_")
+end
